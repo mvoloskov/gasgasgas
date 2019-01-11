36076ae9167666a7ade0389452bdd84fb902a0aa
uyouthe <melvoloskov@ya.ru>
2019-01-11 22:25:11 +0300

    Done

---
+++ index.js
@@ -0,0 +1,13 @@
+const ncu = require('npm-check-updates')
+const { exec } = require('child_process')
+
+ncu.run({
+  packageFile: 'package.json',
+  upgrade: true,
+  silent: true
+}).then(() => {
+  console.log('Upgrade finished. Have fun!')
+  exec('npm i', (err, stdout, stderr) => {
+    console.log(stdout)
+  })
+})
